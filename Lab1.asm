e93813203b4a522036f652d4455f3d9ba41cc362
Tim <40692433+Osipchik@users.noreply.github.com>
2019-09-16 09:12:17 +0300

    Update Lab1.asm

--- Lab1.asm
+++ Lab1.asm
@@ -1,79 +1,54 @@
+
 model small
 .stack 100h
 .data
-a dw 1
-b dw 2
-c dw 3
+a dw 6
+b dw 8
+c dw 10
 d dw 4
 .code
 
 start:
  MOV AX, @data
  MOV DS, AX
-
+;<readABCD>
  MOV AX, a
  MOV BX, b
  MOV CX, c
- MOV DX, d
 
- XOR AX, BX       ; AX = a ^ b
- ADD CX, DX       ; BX = c + d
- CMP AX, CX       ; if a ^ b == c + d
+ AND AX, d        ; AX = a & d
+ OR CX, d         ; CX = c | d
+ CMP AX, CX       ; if a & d == c | d
  JE aXORb
                   ; false
  MOV AX, a
- AND AX, BX       ; AX = a & b
-
- MOV CX, c
- ADD CX, DX       ; CX = c + d
- CMP AX, CX       ; if a & b == c + d
+ AND BX, d        ; BX = b & d
+ ADD BX, AX       ; BX = BX + a
+ CMP AX, c        ; if a + b & d == c
  JE aANDb
                   ; false
  MOV AX, a
  MOV CX, c
 
- OR AX, BX        ; AX = a | b
- OR AX, CX        ; AX = AX | c
- OR AX, DX        ; AX = AX | d
+ XOR AX, b        ; AX = a ^ b
+ AND CX, d        ; CX = c & d
+ OR AX, CX        ; AX = AX | CX
 JMP Finish
 aXORb:
- MOV CX, c
  MOV AX, a
- AND AX, CX       ; AX = a & c
- OR BX, DX        ; BX = b | d
- ADD AX, BX
+ AND AX, c        ; AX = a & c
+ AND BX, d        ; BX = b & d
+ ADD AX, BX       ; AX = AX + BX
 JMP Finish
 aANDb:
  MOV AX, a
  MOV CX, c
- XOR AX, BX       ; AX = a ^ b
- XOR AX, CX       ; AX = AX ^ c
- XOR AX, DX       ; AX = AX ^ d
+ OR AX, b         ; AX = a | b
+ AND CX, d        ; CX = c & d
+ OR AX, CX        ; AX = AX | CX
 Finish:
 
+;<print>
  MOV AH, 4Ch
  INT 21h
 end start
-
-
-
-
-IsEqual:
- MOV CX, c
- ADD CX, 101b     ; CX = c + 5
- MOV AX, a
- CMP CX, AX       ; c + 5 == a
- JE IsTrue
-
- MOV CX, c
- AND AX, CX       ; AX = a & c
- OR AX, DX        ; AX = AX | d
-
- JMP Finish
-IsTrue:
- MOV AX, a
- MOV BX, b
- MUL BX           ; DX:AX = a * b, DX = NULL
- XOR CX, 100b     ; CX = c ^ 4
- ADD AX, CX       ; AX = a * b + CX
-
